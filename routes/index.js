8c218ec075cd43412885749d7688e70b7db6ecaa
saintess1979 <saintess@163.com>
2015-07-23 09:27:50 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.login=    function(req, res) {
+  res.render('login', {title: 'ÓÃ»§µÇÂ½'});
+};
+
+
+
+module.exports = router;
